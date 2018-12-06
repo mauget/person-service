a4de8b169c3cb31a2fad420f0fc06502cfb7e3ab
mauget <mauget@gmail.com>
2018-12-06 09:30:35 -0500

    Removed Person getters/setters to promote good JSON

--- src/person.js
+++ src/person.js
@@ -2,43 +2,10 @@
 
 class Person {
     constructor(id, firstName, lastName, email) {
-        this._id = id;
-        this._firstName = firstName;
-        this._lastName = lastName;
-        this._email = email;
-    }
-
-
-    get id() {
-        return this._id;
-    }
-
-    set id(value) {
-        this._id = value;
-    }
-
-    get firstName() {
-        return this._firstName;
-    }
-
-    set firstName(value) {
-        this._firstName = value;
-    }
-
-    get lastName() {
-        return this._lastName;
-    }
-
-    set lastName(value) {
-        this._lastName = value;
-    }
-
-    get email() {
-        return this._email;
-    }
-
-    set email(value) {
-        this._email = value;
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
     }
 }
 
