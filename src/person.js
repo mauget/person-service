9d525b26b94949cf101eb2a2c51fa02ed5273496
mauget <mauget@gmail.com>
2018-12-06 08:10:05 -0500

    Map Docker container port 3000 to client 3000

--- src/person.js
+++ src/person.js
@@ -2,10 +2,43 @@
 
 class Person {
     constructor(id, firstName, lastName, email) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
+        this._id = id;
+        this._firstName = firstName;
+        this._lastName = lastName;
+        this._email = email;
+    }
+
+
+    get id() {
+        return this._id;
+    }
+
+    set id(value) {
+        this._id = value;
+    }
+
+    get firstName() {
+        return this._firstName;
+    }
+
+    set firstName(value) {
+        this._firstName = value;
+    }
+
+    get lastName() {
+        return this._lastName;
+    }
+
+    set lastName(value) {
+        this._lastName = value;
+    }
+
+    get email() {
+        return this._email;
+    }
+
+    set email(value) {
+        this._email = value;
     }
 }
 
