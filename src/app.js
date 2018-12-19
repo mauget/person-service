296acea47ba158a9ad6e224395320b62f54db463
mauget <mauget@gmail.com>
2018-12-19 10:49:13 -0500

    Pushed example of commented flow annotation format

--- src/app.js
+++ src/app.js
@@ -2,12 +2,18 @@
 
 // @flow
 
+// This module is an example of using the comment-form of flow annotation.
+// The flow server does not strip these annotations.
+// They're identical to normal flow annotations' action.
+
+/*::
 type PersonObj = {
     id: number,
     firstName: string,
     lastName: string,
     email: string
 }
+*/
 
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -20,42 +26,42 @@ const app = new express();
 // register JSON parser middleware
 app.use(bodyParser.json());
 
-app.listen(PORT, (): void => {
+app.listen(PORT, ()/*: void*/ => {
     console.log('Person service listening');
 });
 
 const router = app;
 
-router.get('/',  (req, res): void  => {
+router.get('/',  (req, res)/*: void */ => {
     res.send('Person service listening');
 });
 
-router.get('/persons/:id', (req, res): void => {
+router.get('/persons/:id', (req, res)/*: void*/ => {
     const id = parseInt(req.params.id);
     const result = personRepo.getById(id);
     res.send(result);
 });
 
-router.get('/persons', (req, res): void => {
+router.get('/persons', (req, res)/*: void*/ => {
     const result = personRepo.getAll();
     res.send(result);
 });
 
-router.delete('/persons/:id', (req, res): void => {
+router.delete('/persons/:id', (req, res)/*: void*/ => {
     const id: number = parseInt(req.params.id);
     const result: string = personRepo.remove(id);
     res.send(result);
 });
 
 // Creates a new person at each call
-router.post('/persons', (req, res): void => {
+router.post('/persons', (req, res)/*: void*/ => {
     const person: PersonObj = req.body;
     const result: string = personRepo.create(person);
     res.send(result);
 });
 
 // Updates or creates a person
-router.put('/persons', (req, res): void => {
+router.put('/persons', (req, res)/*: void*/ => {
     const person: PersonObj = req.body;
     const result: string = personRepo.save(person);
     res.send(result);
